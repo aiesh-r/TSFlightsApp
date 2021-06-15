947c3c49333a2f835547867911d04f21490302df
aiesh-r <ishani.rangana123@gmail.com>
2021-06-15 13:31:33 +0530

    resolved errors in HTTPClient module

--- src/app/shared/services/http/flights.service.ts
+++ src/app/shared/services/http/flights.service.ts
@@ -1,55 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Flight } from '../../interfaces/flight.model';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FlightsService {
-  // flights: Flight[] = [
-  //   {
-  //     origin: 'miami',
-  //     departure: 'USA',
-  //     flightNumber: 1,
-  //     departDate: new Date('2021-05-08T23:05:21.222z'),
-  //     arriveDate: new Date('2021-05-06T23:05:21.222z'),
-  //     nonstop: true,
-  //   },
-  //   {
-  //     origin: 'india',
-  //     departure: 'Sri Lanka',
-  //     flightNumber: 234,
-  //     departDate: new Date('2021-05-08T23:05:21.222z'),
-  //     arriveDate: new Date('2021-05-06T23:05:21.222z'),
-  //     nonstop: true,
-  //   },
-  //   {
-  //     origin: 'india',
-  //     departure: 'Sri Lanka',
-  //     flightNumber: 234,
-  //     departDate: new Date('2021-05-08T23:05:21.222z'),
-  //     arriveDate: new Date('2021-05-06T23:05:21.222z'),
-  //     nonstop: true,
-  //   },
-  //   {
-  //     origin: 'india',
-  //     departure: 'Sri Lanka',
-  //     flightNumber: 234,
-  //     departDate: new Date('2021-05-08T23:05:21.222z'),
-  //     arriveDate: new Date('2021-05-06T23:05:21.222z'),
-  //     nonstop: true,
-  //   },
-  // ];
+  // path: string = 'http://localhost:3000';
+  constructor(private http: HttpClient) {
+    this.backEndURL = this.getBackEndUrl();
+  }
 
-  path: string = 'http://localhost:3000';
-  constructor(private http: HttpClient) {}
+  backEndURL: string;
 
   getFlight(): Observable<any> {
-    return this.http.get(this.path + '/flights/');
+    return this.http.get(`${this.backEndURL}/flights/`);
   }
 
   addFlight() {}
 
   dleteFlight() {}
+
+  getBackEndUrl(): string {
+    const segements = document.URL.split('/');
+    const reggie = new RegExp(/localhost/);
+    return reggie.test(segements[2])
+      ? 'http://localhost:3000'
+      : 'https://shrouded-ravine-23045.herokuapp.com';
+  }
 }
